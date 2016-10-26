4af5bc6432368d6d5f6439c9634b1e36ddbf7dd2
HuangXingBin <356578436@qq.com>
2016-10-26 17:16:16 +0800

    先写UI#1

--- src/appConstants/urlConfig.js
+++ src/appConstants/urlConfig.js
@@ -2,10 +2,10 @@
 export const logoutUrl = '/Api/index.php/Index/logout';
 
 // 登录页
-export const loginUrl = 'login/index.html';
-
+export const loginUrl = '/api/index.php/Index/index';
+export const loginPage = '/public/login/index.html';
 // 默认头像
-export const defaultAvatar = '/img/avatar.jpg';
+export const defaultAvatar = '/public/avatar.jpg';
 
 // 修改密码
 export const modifyPasswordUrl = '/Base/modifyPassWord';
@@ -13,6 +13,4 @@ export const modifyPasswordUrl = '/Base/modifyPassWord';
 // 根据开发环境还是生产环境决定路由
 export const routeBase = process.env.NODE_ENV !== 'production' ? '/' : '/';
 
-// 获取当前登录用户的信息
-export const getAdminUrl = '/Api/index.php/Index/getUserInfo';
 

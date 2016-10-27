6780cb87c49aaeb75281a65948b79a41710e5ece
enjing <578951992@qq.com>
2016-10-27 21:07:11 +0800

    change config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -26,8 +26,8 @@ module.exports = {
 		'./src/app.js'
 	],
 	output: {
-		// path: path.join(__dirname, 'public'),
-		path: path.join(__dirname, '../../Documents/小金/root/public'),
+		 path: path.join(__dirname, 'public'),
+//		path: path.join(__dirname, '../../Documents/小金/root/public'),
 		// filename: '[chunkhash].js'
 		filename: 'bundle.js'
 	},
